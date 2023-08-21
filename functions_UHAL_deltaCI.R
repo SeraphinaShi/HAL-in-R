27c7cfdb0ea5588f2bcecd3360b8b2d8d5f6de77
SeraphinaShi <junmingshi67@gmail.com>
2023-08-21 16:04:25 -0700

    add functions of calculating inference using delta methods and finish first example

--- functions_UHAL_deltaCI.R
+++ functions_UHAL_deltaCI.R
@@ -14,7 +14,7 @@
 #' @param X An input \code{matrix} with dimensions number of observations -by-
 #'  number of covariates that will be used to derive the design matrix of basis
 #'  functions.
-#' @param Y A \code{numeric} vector of observations of the outcome variable.
+#' @param Y A \code{numeric} vector of the outcome variable of observations. 
 #' @param fit_init The initial HAL fit object from the output list of \code{undersmooth_init}.
 #' @param Nlam Number of lambda candidates. The sequence ranges from \code{fit_init$lambda_star} to
 #' \code{fit_init$lambda_star*10^(-3)}.
@@ -49,12 +49,12 @@ undersmooth_hal <- function(X,
   }
   
   
-  if (family != "binomial"){
-    preds_init <- predict(fit_init, new_data = X)
-    pred_mat <- predict(us_fit, fit_init$x_basis)
-  }else {
+  if (family == "binomial"){
     preds_init <- predict(fit_init, new_data = X, type = "response")
     pred_mat <- predict(us_fit, fit_init$x_basis, type = "response")
+  }else {
+    preds_init <- predict(fit_init, new_data = X)
+    pred_mat <- predict(us_fit, fit_init$x_basis)
   }
   resid_mat <- pred_mat - Y
   
@@ -108,8 +108,8 @@ undersmooth_hal <- function(X,
 #' Select the max of the mean.
 #' @param basis_mat The selected basis matrix from initial fit for undersmoothing,
 #'  obtained from the output list of \code{undersmooth_init}.
-#' @param resid_mat The residual matrix with each column the residuals correspongding to a lambda.
-#' @param sd_est A numeric vector containing the sd of each column of \code{basis_mat}.
+#' @param resid_mat The residual \code{numeric} with each column the residuals correspongding to a lambda.
+#' @param sd_est A \code{numeric} vector containing the sd of each column of \code{basis_mat}.
 #' @param Nlam Number of lambda candidates.
 #' @param us_fit The \code{glmnet} fit of the sequence of candidate lambdas.
 
@@ -124,3 +124,313 @@ get_maxscore <- function(basis_mat, resid_mat, sd_est, Nlam, us_fit){
   max_score <- apply(abs(score_all), 1, max, na.rm=T)
   return(max_score)
 }
+
+
+
+###############################################################################
+#'  calculate ATE estimate and a confidence interval using delta method.
+#'
+#' @details This is an inference function for average treatment effect (ATE). 
+#'  With provided X, Y, and their corresponding HAL fit, it will calculate the 
+#'  ATE estimate, SE, and confidence intervals using delta method. 
+#' @param X An input \code{matrix} with dimensions number of observations -by-
+#'  number of covariates that will be used to derive the design matrix of basis
+#'  functions.The same \code{X} used in \code{fit_hal} function.
+#' @param Y A \code{numeric} vector of the outcome variable of observations. 
+#'  The same \code{Y} used in \code{fit_hal} function.
+#' @param treatment A \code{character} specifying the treatment column name in \code{X}
+#' @param condition A \code{character} specifying the condition column name in 
+#'  \code{X}, only used when estimating the CATE.
+#' @param condition_level A \code{character} or a \code{numeric} specifying the 
+#'  condition level, only used when estimating the CATE.
+#' @param hal_fit The HAL fit object from the output of \code{fit_hal} function.
+#' @param alpha The statistical significance level for the influence curve.  
+#' @param family A \code{character} or a \code{\link[stats]{family}} object
+#'  (supported by \code{\link[glmnet]{glmnet}}) specifying the error/link
+#'  family for a generalized linear model. See more details in \code{fit_hal}.
+ATE_inferece <- function(X, Y, treatment, condition = NA, condition_level = NA, hal_fit, alpha = 0.05, family = "gaussian"){
+  
+  n = length(Y)
+  treat_idx = which(colnames(X) == treatment)
+  
+  X = as.matrix(X)
+  
+  if(! is.na(condition)){
+    cond_idx = which(colnames(X) == condition)
+    obj_cond_idx = X[, condition] == condition_level
+    X_c = X[obj_cond_idx, ]
+    Y_c = Y[obj_cond_idx]
+  } else {
+    X_c = X
+    Y_c = Y
+  }
+  
+  # 1. get estimate
+  ## intervene to set A=a and generate the counterfactual outcomes Y.a
+  X.1 = as.matrix(X_c) 
+  X.1[,treat_idx] = 1
+  Y.1 = predict(hal_fit, new_data = X.1)
+  
+  X.0 = as.matrix(X_c) 
+  X.0[,treat_idx] = 0
+  Y.0 = predict(hal_fit, new_data = X.0)
+  
+  ## Estimation 
+  ATE_est = mean(Y.1 - Y.0)
+  
+  # 2. get inferences
+  ## 2.1. influence curve of beta
+  coef <- hal_fit$coefs
+  basis_mat <- cbind(1, as.matrix(hal_fit$x_basis))
+  
+  nonzero_idx <- which(coef != 0)
+  
+  coef_nonzero <- coef[nonzero_idx]
+  basis_mat_nonzero <- as.matrix(basis_mat[, nonzero_idx])
+  
+  if(family == 'binomial'){
+    Y_hat = predict(hal_fit, new_data = X, type = "response")
+  } else {
+    Y_hat = predict(hal_fit, new_data = X)
+  }
+  
+  IC_beta = cal_IC_for_beta(X = basis_mat_nonzero,
+                            Y = Y,
+                            Y_hat = Y_hat,
+                            beta_n = coef_nonzero,
+                            family = family)
+  
+  if(! is.na(condition)){
+    IC_beta = IC_beta[, obj_cond_idx]
+  }
+  
+  ## 2.2. influence curve of point estimate
+  x_basis.1 <- make_design_matrix(X.1, hal_fit$basis_list, p_reserve = 0.75)
+  x_basis_nonzero.1 <- as.matrix(cbind(1, x_basis.1)[, nonzero_idx])
+  
+  IC_EY.1 <- cal_IC_for_EY(X = x_basis_nonzero.1, 
+                           beta_n = coef_nonzero, 
+                           IC_beta = IC_beta,
+                           family = family)
+  
+  x_basis.0 <- make_design_matrix(X.0, hal_fit$basis_list, p_reserve = 0.75)
+  x_basis_nonzero.0 <- as.matrix(cbind(1, x_basis.0)[, nonzero_idx])
+  
+  IC_EY.0 <- cal_IC_for_EY(X = x_basis_nonzero.0, 
+                           beta_n = coef_nonzero, 
+                           IC_beta = IC_beta,
+                           family = family)
+  
+  IC_ATE = IC_EY.1 - IC_EY.0
+  
+  ## 2.3. standard error & confidence intervals
+  SE_ATE = sqrt(var(IC_ATE)/n)
+  
+  ci_lwr_ATE <- ATE_est - abs(qnorm(alpha/2)) * SE_ATE
+  ci_upr_ATE <- ATE_est + abs(qnorm(alpha/2)) * SE_ATE
+  
+  if(is.na(condition)){
+    Target_param = paste0("E[Y_1] - E[Y_0]")
+    returns = list(Target_parameter = paste0("E[Y_1] - E[Y_0]"),
+                   Estimate = ATE_est,
+                   SE = SE_ATE,
+                   CI = c(ci_lwr_ATE, ci_upr_ATE))
+  } else {
+    Target_param = paste0("E[Y_1|", condition, "=", condition_level, "] - E[Y_0|", condition, "=", condition_level, "]")
+  }
+  
+  returns = list(Target_parameter = Target_param,
+                 Estimate = ATE_est,
+                 SE = SE_ATE,
+                 CI = c(ci_lwr_ATE, ci_upr_ATE))
+  
+  return(returns)
+
+}
+
+###############################################################################
+#'  calculate the counterfactual outcome mean estimate and a confidence interval using delta method.
+#'
+#' @details This is an inference function for counterfactual outcome mean. 
+#'  With provided X, Y, and their corresponding HAL fit, it will calculate the 
+#'  counterfactual outcome mean estimate, SE, and confidence intervals using delta method. 
+#' @param X An input \code{matrix} with dimensions number of observations -by-
+#'  number of covariates that will be used to derive the design matrix of basis
+#'  functions.The same \code{X} used in \code{fit_hal} function.
+#' @param Y A \code{numeric} vector of the outcome variable of observations. 
+#'  The same \code{Y} used in \code{fit_hal} function.
+#' @param treatment A \code{character} specifying the treatment column name in \code{X}
+#' @param treatment_level A \code{character} or a \code{numeric} value specifying 
+#'  the counterfactual treatment level.
+#' @param condition A \code{character} specifying the condition column name in 
+#'  \code{X}, only used when estimating the CATE.
+#' @param condition_level A \code{character} or a \code{numeric} specifying the 
+#'  condition level, only used when estimating the CATE.
+#' @param hal_fit The HAL fit object from the output of \code{fit_hal} function.
+#' @param alpha The statistical significance level for the influence curve.  
+#' @param family A \code{character} or a \code{\link[stats]{family}} object
+#'  (supported by \code{\link[glmnet]{glmnet}}) specifying the error/link
+#'  family for a generalized linear model. See more details in \code{fit_hal}.
+CounterfactualMean_inferece <- function(X, Y, treatment, treatment_level, condition = NA, condition_level = NA, hal_fit, alpha = 0.05, family = "gaussian"){
+  
+  n = length(Y)
+  treat_idx = which(colnames(X) == treatment)
+  
+  if(! is.na(condition)){
+    cond_idx = which(colnames(X) == condition)
+    obj_cond_idx = X[, condition] == condition_level
+    X_c = X[obj_cond_idx, ]
+    Y_c = Y[obj_cond_idx]
+  } else {
+    X_c = X
+    Y_c = Y
+  }
+  
+  # 1. get estimate
+  ## intervene to set A=a and generate the counterfactual outcomes Y.a
+  X.a = as.matrix(X_c) 
+  X.a[,treat_idx] = treatment_level
+  Y.a = predict(hal_fit, new_data = X.a)
+  
+  ## Estimation 
+  CounterfactualMean_est = mean(Y.a)
+  
+  # 2. get inferences
+  ## 2.1. influence curve of beta
+  coef <- hal_fit$coefs
+  basis_mat <- cbind(1, as.matrix(hal_fit$x_basis))
+  
+  nonzero_idx <- which(coef != 0)
+  
+  coef_nonzero <- coef[nonzero_idx]
+  basis_mat_nonzero <- as.matrix(basis_mat[, nonzero_idx])
+  
+  if(family == 'binomial'){
+    Y_hat = predict(hal_fit, new_data = X, type = "response")
+  } else {
+    Y_hat = predict(hal_fit, new_data = X)
+  }
+  
+  IC_beta = cal_IC_for_beta(X = basis_mat_nonzero,
+                            Y = Y,
+                            Y_hat = Y_hat,
+                            beta_n = coef_nonzero,
+                            family = family)
+  
+  if(! is.na(condition)){
+    IC_beta = IC_beta[, obj_cond_idx]
+  }
+  
+  ## 2.2. influence curve of point estimate
+  x_basis.a <- make_design_matrix(X.a, hal_fit$basis_list, p_reserve = 0.75)
+  x_basis_nonzero.a <- as.matrix(cbind(1, x_basis.a)[, nonzero_idx])
+  
+  IC_EY.a <- cal_IC_for_EY(X = x_basis_nonzero.a, 
+                           beta_n = coef_nonzero, 
+                           IC_beta = IC_beta,
+                           family = family)
+  
+  ## 2.3. standard error & confidence intervals
+  SE = sqrt(var(IC_EY.a)/n)
+  
+  ci_lwr <- CounterfactualMean_est - abs(qnorm(alpha/2)) * SE
+  ci_upr <- CounterfactualMean_est + abs(qnorm(alpha/2)) * SE
+  
+
+  
+  if(is.na(condition)){
+    returns = list(Target_parameter = paste0("EE[Y|A=", treatment_level, ", W]"),
+                   Estimate = CounterfactualMean_est,
+                   SE = SE,
+                   CI = c(ci_lwr, ci_upr))
+  } else {
+    returns = list(Target_parameter = paste0("EE[Y|A=", treatment_level, ",", ondition, "=", condition_level, " W]"),
+                   Estimate = CounterfactualMean_est,
+                   SE = SE,
+                   CI = c(ci_lwr, ci_upr))
+  }
+  
+  return(returns)
+  
+}
+
+###############################################################################
+#'  HAL based inferences helper function for calculating the influence curve of beta
+#'
+#' @details Calculate the influence curve of beta, $D_p(\Phi,Y)$
+#' 
+#' @param X The basis matrix with non-zero coefficients from initial fit.
+#' @param Y A \code{numeric} vector of the outcome variable of observations. 
+#'  The same \code{Y} used in \code{fit_hal} function.
+#' @param Y_hat A \code{numeric} vector of the predicted outcome variable of 
+#'  observations from \code{predict} function using on the HAL fit object. 
+#' @param beta_n A \code{numeric} vector containing the non-zero coefficients.
+#' @param family A \code{character} or a \code{\link[stats]{family}} object
+#'  (supported by \code{\link[glmnet]{glmnet}}) specifying the error/link
+#'  family for a generalized linear model. See more details in \code{fit_hal}.
+
+cal_IC_for_beta <- function(X, Y, Y_hat, beta_n, family = 'binomial'){
+  n <- dim(X)[1] 
+  p <- length(beta_n)
+  if (!is.matrix(X)) X <- as.matrix(X)
+  
+  # 1. calculate score: X'(Y - phi(X))
+  res <- Y-Y_hat
+  score <- sweep(t(X), 2, res, `*`)
+  
+  # 2. calculate d_phi, the derivative of phi:
+  if(family == 'binomial'){
+    d_phi_scaler <- as.vector(exp(- beta_n %*% t(X)) / ((1 + exp(- beta_n %*% t(X)))^2))
+    d_phi <- sweep(X, 1, d_phi_scaler, `*`)
+  } else {
+    d_phi = - X
+  }
+  
+  # 3. -E_{P_n}(X d_phi)^(-1)
+  tmat <- t(X) %*% d_phi / n
+  if(! is.matrix(try(solve(tmat), silent = TRUE))){
+    return(NA)
+  }
+  tmat <- -solve(tmat)
+  
+  # 4. calculate influence curves
+  IC <- tmat %*% score
+  
+  return(IC)
+}
+
+
+###############################################################################
+#'  HAL based inferences helper function for calculating the influence curve of Y
+#'
+#' @details Calculate the influence curve of Y, 
+#'  $IC_{\psi} = \frac{d}{d\beta}\psi_{P_n}(\Phi) * D_p(\Phi,Y) $
+#' 
+#' @param X A basis matrix with non-zero coefficients from initial fit.
+#' @param beta_n A \code{numeric} vector containing the non-zero coefficients.
+#' @param IC_beta A \code{numeric} vector containing the influence curves of beta.
+#' @param family A \code{character} or a \code{\link[stats]{family}} object
+#'  (supported by \code{\link[glmnet]{glmnet}}) specifying the error/link
+#'  family for a generalized linear model. See more details in \code{fit_hal}.
+cal_IC_for_EY <- function(X, beta_n, IC_beta, family = 'binomial'){
+  if (!is.matrix(X)) X <- as.matrix(X)
+  
+  if(family == 'binomial'){
+    d_phi_scaler_new <- as.vector(exp(- beta_n %*% t(X)) / ((1 + exp(- beta_n %*% t(X)))^2))
+    d_phi_new <- sweep(X, 1, d_phi_scaler_new, `*`)
+  } else {
+    d_phi_new = X
+  }
+  
+  IC = diag(d_phi_new %*% IC_beta)
+  
+  return(IC)
+}
+
+
+
+
+
+
+
+
